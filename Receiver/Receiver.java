eeea48edf49b9f7da005f414b05a14c775fe2d32
DvdMgr <magrin.davide@gmail.com>
2014-12-16 21:42:32 +0100

    First draft of a receiver socket

--- Receiver/Receiver.java
+++ Receiver/Receiver.java
@@ -1,19 +1,24 @@
-import java.net.*;
+import java.net.*; // Sockets and Datagram utils
+import Utils.PacketAnalyzer;
 
 public class Receiver {
 
-  // Stub method
-  public static void main(String[] args) {
+  static int listeningPort = 65433;
+  static int totalPacketLength = 32+32+32+512; // Destination address + Port and codes + SN + Payload
 
-    // Initialize the UDP socket, listening on port 65432
-    DatagramSocket socket = new DatagramSocket(65432);
+  public static void main(String[] args) throws Exception { // TODO Refine Exception catching
 
-    byte[] receivedData = new byte[6];
+    // Initialize the UDP socket, listening on port 65433
+    DatagramSocket socket = new DatagramSocket(listeningPort);
+
+    byte[] receivedData = new byte[512];
     DatagramPacket datagram = new DatagramPacket(receivedData, receivedData.length);
     socket.receive(datagram);
 
     for (int i = 0; i < receivedData.length; i++ )
-    System.out.print(receivedData[i] + " ");
+      System.out.print(receivedData[i] + " ");
+
+    System.out.println("");
 
     return;
   }
