a6b9a1e1974c912cf1396da4ee83d6eb45b16b6e
DvdMgr <magrin.davide@gmail.com>
2014-12-16 21:42:13 +0100

    Created a dummy sender to simulate data sending

--- Receiver/DummySender.java
+++ Receiver/DummySender.java
@@ -2,16 +2,21 @@ import java.net.*;  // Sockets and Datagram utils
 
 public class DummySender {
 
+  static int sendingPort = 65433;
+
   public static void main(String[] args) throws Exception {
 
     // Initialize the DatagramSocket
     DatagramSocket socket = new DatagramSocket();
 
-    byte[] data = {1, 2, 3, 4, 5, 6};
+    String message = "Here is the message";
+
+    byte[] data = message.getBytes();
+
     InetAddress localhost = InetAddress.getLocalHost();
-    socket.send(new DatagramPacket(data, data.length, localhost, 65432));
+    socket.send(new DatagramPacket(data, data.length, localhost, sendingPort));
 
     return;
   }
-  
+
 }
