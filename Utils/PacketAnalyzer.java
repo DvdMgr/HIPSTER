be7af6177a8b98c69f76e9517a0c5c97644058d1
DvdMgr <magrin.davide@gmail.com>
2014-12-16 22:05:44 +0100

    Added getter methods, variables and commented

--- Utils/PacketAnalyzer.java
+++ Utils/PacketAnalyzer.java
@@ -1,9 +1,9 @@
 /*
-  This class will analyze datagrams, extracting information from the protocol-defined header.
+  This class will analyze the datagram payload, extracting information from the protocol-defined header.
 
-  The same instance can be reused via the analyze(Datagram datagram) method, or multiple instances can be used
-  at the same time. After running analyze or creating a new analyzer, the header can be inspected using the get
-  methods.
+  The same instance can be reused via the analyze method, or multiple instances can be used
+  at the same time. After running analyze or creating a new analyzer, the header and the data can be
+  inspected using the get methods.
 
   Here is the reference of the packet structure:
 
@@ -35,16 +35,68 @@ import java.net.*;
 
 public class PacketAnalyzer {
 
-  // Public constructor
-  public PacketAnalyzer(DatagramPacket datagram) {
+  /*
+    Public constructors
+  */
+
+  // The empty constructor will return 0 for all ints and false for all booleans unless analyze is used
+  public PacketAnalyzer() {
 
   }
 
-  // Variables
+  public PacketAnalyzer(byte[] packet) {
+    analyze(packet);
+  }
+
+  // This method is used to analyze a new packet (using the same instance of the PacketAnalyzer)
+  public analyze(byte[] packet) {
+    // TODO This method has to fill in the values of the fields of the packet correctly
+  }
+
+  /*
+    Getter methods
+  */
+
+  // TODO implement getDestinationAddress (what type should it return?)
+
+  public int getDestinationPort() {
+    return destinationPort;
+  }
+
+  public int getDataLength() {
+    return dataLength;
+  }
+
+  public boolean isRegularDatagram() {
+    return isRegularDatagram;
+  }
+
+  public boolean isAck() {
+    return isAck;
+  }
+
+  public boolean isEtx() {
+    return isEtx;
+  }
+
+  public int getSequenceNumber() {
+    return sequenceNumber;
+  }
+
+  public byte[] getData() {
+    return data;
+  }
+
+  /*
+    Variables
+  */
+
+  // private InetAddress destinationAddress; // TODO Decide whether we want this in InetAddress form or something else
+  private int destinationPort;
   private int dataLength;
-  private boolean regularDatagram;
-  private boolean ack;
-  private boolean etx;
+  private boolean isRegularDatagram;
+  private boolean isAck;
+  private boolean isEtx;
   private int sequenceNumber;
   private byte[] data;
 }
