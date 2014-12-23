68eee5f49b73f1da08a981fb31fe1c8e4b7be471
DvdMgr <magrin.davide@gmail.com>
2014-12-23 10:35:55 +0100

    Cleaned up code, commented, args handling added
    Various goodies were added, and a more readable output is now
    generated, as we are preparing for the Christmas deadline.

--- src/Receiver.java
+++ src/Receiver.java
@@ -6,34 +6,66 @@
 
 import java.net.*;    // Sockets and Datagram utils
 import java.util.*;   // Maps and useful stuff
-import java.io.*;
+import java.io.*;     // Support for writing files
 
 // TODO Clarify addresses and ports for the channel and the source.
 // TODO Decide whether to use a TreeMap or a HashMap followed by a TreeMap for sorting
 // TODO Clean up this mess
 public class Receiver {
 
-  static String filename = "File";
+  static String filename = "ReceivedFile";
 
-  static int udpListenPort = 65433;// 65433;                 // These two ports handle communication with the channel
-  static int udpSendPort = 65432;//65432;
+  static int udpListenPort = 65433;                  // These two ports handle communication with the channel
+  static int udpSendPort = 65432;
 
   static int hipsterListenPort = 65433;              // These two ports handle communication with the sender
   static int hipsterSendPort = 65431;
 
-  static final int MAXIMUM_DATAGRAM_SIZE = 2048;    // FIXME Totally arbitrary "Big number"
+  static final int MAXIMUM_DATAGRAM_SIZE = 2048;    // This buffer will be trimmed according to the size of the datagram
 
   static InetAddress sourceAddress;                 // TODO Set these dynamically?
   static InetAddress channelAddress;
 
+  private static final String USAGE = "USAGE:\n\t" +
+  "Receiver [-c channel_IP] [-d destination_IP:Port] [-p Port] [-f output_file]" +
+  "\n\nBy default all addresses are 'localhost'.\n" +
+  "The default port this program listens on is 65433.\n" +
+  "The default port for the receiver is 65431.";
+
   public static void main(String[] args) throws Exception { // TODO Refine Exception catching
 
-    // TODO Use nice args handling to set parameters
-    if (args.length == 1)
-      filename = args[0];
+    // By default these addresses are localhost.
+    sourceAddress = InetAddress.getLocalHost();
+    channelAddress = InetAddress.getLocalHost();
 
-    sourceAddress = InetAddress.getByName("82.48.46.170"); //getLocalHost();     // TODO We should use the actual address here
-    channelAddress = InetAddress.getByName("79.7.45.120"); //getLocalHost();
+    // Parse command line arguments
+    for(int i = 0; i < args.length; i++)
+    {
+      if ("-h".equals(args[i])) {
+        System.out.println(USAGE);
+        return;
+      } else if ("-c".equals(args[i])) {
+        // the next string is the channel address
+        i++;
+        channelAddress = InetAddress.getByName(args[i]);
+      } else if ("-d".equals(args[i])) {
+        // the next string is the destination address
+        i++;
+        String[] sep = args[i].split(":");
+        sourceAddress = InetAddress.getByName(sep[0]);
+        if (sep.length > 1) {
+          hipsterSendPort = Integer.parseInt(sep[1]);
+        }
+      } else if ("-p".equals(args[i])) {
+        // the next string is my port
+        i++;
+        udpListenPort = Integer.parseInt(args[i]);
+      } else if ("-f".equals(args[i])) {
+        // the next string is the filename
+        i++;
+        filename = args[i];
+      }
+    }
 
     // Initialize the UDP sockets
     DatagramSocket receiverSocket = new DatagramSocket(udpListenPort);
@@ -42,14 +74,19 @@ public class Receiver {
     // Prepare the map that will be used to store the file
     Map<Integer, byte[]> map = new HashMap<Integer, byte[]>();
 
-    boolean[] checklist = new boolean[3180];
+    // This checklist is used to keep track of which packets we have received
+    boolean[] checklist = new boolean[10];
 
+    // Variables that keep track of the speed at which we receive data
     long startTime = System.currentTimeMillis();
     long dataReceived = 0;
 
-    int lastSn = 0;
+    // This will be the last sequence number
+    int etxSn = 0;
 
-    // Start executing the algorithm
+    /*
+     * Start executing the algorithm
+     */
     boolean waitingForData = true;
 
     while (waitingForData) {
@@ -62,16 +99,26 @@ public class Receiver {
       hipsterPacket = hipsterPacket.fromDatagram(datagram);
       int sn = hipsterPacket.getSequenceNumber();
       byte[] data = hipsterPacket.getPayload();
-      dataReceived += datagram.getLength();
+      dataReceived += datagram.getLength();       // Counter of the received bytes
 
-      // Print sn of the received packet
-      System.out.println("Received packet of sequence number: " + sn);
-      checklist[sn] = true;
+      // Print sn of the received packet (debugging purposes)
+      //System.out.println("Received packet of sequence number: " + sn);
+
+      // Mark packet as received in the checklist
+      if (sn < checklist.length) {
+        checklist[sn] = true;
+      } else {
+        System.out.println("Expanding checklist...");
+        boolean[] newchecklist = new boolean[2*checklist.length];
+        System.arraycopy(checklist, 0, newchecklist, 0, checklist.length);
+        checklist = newchecklist;
+        checklist[sn] = true;
+      }
 
       // Check whether the packet has the ETX flag
       if (hipsterPacket.isEtx()) {
         waitingForData = false;
-        lastSn = sn;
+        etxSn = sn;
       }
       else {
         // Add the packet to the map using the SN as key
@@ -92,25 +139,34 @@ public class Receiver {
     long elapsed = System.currentTimeMillis() - startTime;
     long speed = dataReceived / elapsed;
 
-    System.out.printf("Bytes received: " + dataReceived);
+    System.out.println("Bytes received: " + dataReceived);
     System.out.println("Elapsed time: " + elapsed + "ms (" + speed +
     "KBps)");
 
+    // Print some debug info
+    System.out.println("Etx packet had SN: " + etxSn);
     int storedMappings = map.size();
     System.out.println("Number of mappings stored: " + storedMappings);
 
-    for (int i = 0; i < lastSn; i++)
-      if (checklist[i] == false)
+    System.out.println(lostPackets + " packets lost in total");
+
+    int lostPackets = 0;
+    for (int i = 0; i < etxSn; i++)
+      if (checklist[i] == false) {
         System.out.println("Packet " + i + " was not received");
+        lostPackets++;
+      }
 
-    // We use ByteArrayOutputStream for testing
-    // Use FileOutputStream for real application (remember that memory is limited)
+    /*
+     * Reorder and write output file
+     */
 
-    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-
-    // Order by adding elements in a treemap
+    // Order elements by adding received data in a treemap
     Map<Integer, byte[]> orderedMap = new TreeMap<Integer, byte[]>();
     orderedMap.putAll(map);
+    map = null;   // Free up memory space
+
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
     for(Map.Entry<Integer,byte[]> entry : orderedMap.entrySet()) {
       Integer integerKey = entry.getKey();
