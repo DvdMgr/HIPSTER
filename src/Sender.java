08e0dcafc750988fddd4ee4118e8bdc655cd8eb6
electricant <electricant@anche.no>
2014-12-24 09:00:21 +0100

    Now the sender is multithreaded.
    It can send data and receive acks simultaneously

--- src/Sender.java
+++ src/Sender.java
@@ -6,7 +6,7 @@
 
 import java.net.*; // Socket and datagram stuff
 import java.io.*;
-import java.util.Arrays;
+import java.util.*; // Vector Arrays and others
 
 public class Sender {
 	private static final String USAGE = "USAGE:\n\t" +
@@ -26,8 +26,6 @@ public class Sender {
 	private static boolean MICHELE_MODE = true;
 	private static final int SENDER_PAUSE = 20;  // ms
 
-	// this socket is used by both threads
-	private static DatagramSocket UDPSock;
 	// runtime options. See USAGE variable
 	private static String fileName = "";
 	private static InetAddress chAddr = InetAddress.getLoopbackAddress();
@@ -70,12 +68,15 @@ public class Sender {
 			System.out.println(USAGE);
 			return;
 		}
-		// initialize some data that will be used later
-		UDPSock = new DatagramSocket(myPort);
-		UDPSock.setSoTimeout(ACK_TIMEOUT);
+		// initialize some data
+		DatagramSocket UDPSock = new DatagramSocket(myPort);
 		FileInputStream inFstream = new FileInputStream(inFile);
+		ListenerThread ackListener = new ListenerThread(UDPSock);
+		ackListener.start();
 		// everythig correctly initialized. Greet the user
 		System.out.println("Listening on port: " + myPort);
+		// need to load the file in memory
+
 		// take time into account (used for statistics)
 		long startTime = System.currentTimeMillis();
 		byte[] buf = new byte[PAYLOAD_SIZE];
@@ -93,8 +94,7 @@ public class Sender {
 			read = inFstream.read(buf);
 			if (MICHELE_MODE == true)
 				Thread.sleep(SENDER_PAUSE);
-			else if ((sn % WINDOW_SIZE == 0) || (read <= 0))
-				processACKs();
+			//else if ((sn % WINDOW_SIZE == 0) || (read <= 0))
 		}
 		// send an ETX packet to close the connection
 		HipsterPacket pkt = new HipsterPacket();
@@ -137,21 +137,41 @@ public class Sender {
 
 		return ret;
 	}
+}
+/*
+ * This thread is responsible for receiving processing acks
+ */
+class ListenerThread extends Thread {
+	
+	private DatagramSocket rSock;
+	public Vector<Integer> acked;
 
-	private static void processACKs() throws IOException {
-		for(int count = 0; count < WINDOW_SIZE; count++) {
-			DatagramPacket aPacket = new DatagramPacket(
+	ListenerThread() {
+		throw new IllegalArgumentException("I need a socket!");
+	}
+	
+	ListenerThread(DatagramSocket theSocket) {
+		rSock = theSocket;
+		acked = new Vector<Integer>();
+	}
+
+	public void run() {
+		while (true) { // forever
+			DatagramPacket received = new DatagramPacket(
 				new byte[HipsterPacket.headerLength],
 				HipsterPacket.headerLength);
 			try {
-				UDPSock.receive(aPacket);
-			} catch (SocketTimeoutException ste) {
-				System.out.println("\nreceive() timed out");
-				return; // there's no point to wait longer
+				rSock.receive(received);
+				HipsterPacket ack = new HipsterPacket().fromDatagram(received);
+				if (ack.isAck()) {
+					System.out.print("\rACK for: " +
+						ack.getSequenceNumber());
+					acked.add(ack.getSequenceNumber());
+				}
+			} catch (Exception ex) {
+				System.out.println("WARNING: " + ex);
+				System.out.println("The exception will be bravely ignored.");
 			}
-			HipsterPacket ack = new HipsterPacket().fromDatagram(aPacket);
-			if (ack.isAck())
-				System.out.print("\rACK for: " + ack.getSequenceNumber());
 		}
 	}
 }
